ea2151ed9dc5125eabe9dc0680ec9e0fc9115d51
Neeraj T <165806591+Neerajdec2005@users.noreply.github.com>
2025-01-15 11:30:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (85.98%) - LeetHub

---
+++ 2429-minimize-xor/2429-minimize-xor.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int k=__builtin_popcount(num2);
+        int x=0;
+        for(int i=31;i>=0 && k;i--){
+            if(num1&(1<<i)){
+                k--;
+                x+=(1<<i);
+            }
+        }
+        for(int i=0;i<32 && k;i++){
+            if((num1&(1<<i))==0){
+                k--;
+                x+=(1<<i);
+            }
+        }return x;
+    }
+};
